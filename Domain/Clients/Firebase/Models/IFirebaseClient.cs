896c39f9b297d46d2bf6a70f78a932ef182c75df
Gediminas <gediminas.buda1@gmail.com>
2021-10-17 13:15:56 +0300

    Created Domain Layer, FirebaseClient Folder

---
+++ Domain/Clients/Firebase/Models/IFirebaseClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Clients.Firebase.Models
+{
+    public interface IFirebaseClient
+    {
+        Task<FirebaseSignUpResponse> SignUpAsync(string email, string password);
+        Task<FirebaseSignInResponse> SignInAsync(string email, string password);
+
+    }
+}
